09101f2db8abd34a6550b1e4ca53aa5a1d7eda0e
anuraghazra <hazru.anurag@gmail.com>
2019-11-17 13:25:51 +0530

    chore: updated push.sh

--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ git config --global user.email "no-reply@canvasfun.io"
 git config --global user.name "CanvasFun Bot"
 git branch -d $BRANCH_NAME || true
 git checkout -b $BRANCH_NAME
-git add ./README.md
+git add --all
 git commit --message "Auto-update README" || exit 0
 git remote add origin-$BRANCH_NAME https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
 git push --force --quiet --set-upstream origin-$BRANCH_NAME $BRANCH_NAME
